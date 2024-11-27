ca4f9bed684f6e3f57ae4b256d244c120fd7765a
Sreehari Pulickamadhom Sreedhar <orectique@gmail.com>
2024-11-28 02:31:42 +1100

    generate build

---
+++ docs/_import/components/download_graph.fc3f8d6e.js
@@ -0,0 +1,11 @@
+import * as htmlToImage from "../../_npm/html-to-image@1.11.11/17ad95e3.js";
+
+export function download_graph(plot) {
+  htmlToImage.toPng(plot, { pixelRatio: 2 })
+  .then(function (dataUrl) {
+    var link = document.createElement('a');
+    link.download = 'alga_graphic.png';
+    link.href = dataUrl;
+    link.click();
+  });
+}
