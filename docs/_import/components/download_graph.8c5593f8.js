1993c2d2d69008250d747abdd314ba036e7aead3
spulick <sreehari.pulickamadhom@alga.asn.au>
2024-12-10 11:30:25 +1100

    update background

--- docs/_import/components/download_graph.8c5593f8.js
+++ docs/_import/components/download_graph.8c5593f8.js
@@ -1,4 +1,4 @@
-import * as htmlToImage from "npm:html-to-image";
+import * as htmlToImage from "../../_npm/html-to-image@1.11.11/17ad95e3.js";
 
 export function download_graph(plot) {
   htmlToImage.toPng(plot, { pixelRatio: 2, backgroundColor: "white" })
